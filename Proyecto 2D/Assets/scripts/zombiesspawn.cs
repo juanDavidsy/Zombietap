ee9bac5450b71dcfbb9016cc3939030ea4684379
unknown <SENA@MEDAPRCCSFSD338.FormacionSena.RED>
2022-06-16 17:10:24 -0500

    Dia 3

--- Proyecto 2D/Assets/scripts/zombiesspawn.cs
+++ Proyecto 2D/Assets/scripts/zombiesspawn.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class zombiesspawn : MonoBehaviour
 {
-    public Transform spawnpoint;
+    Vector3 zombiespawn;
+    float  tiempodesalida;
+    public Transform ritghlimit, leftlimit;
+    public float tiempodeaparicion;
     public GameObject [] zombies;
     
     private void Start()
     {
-        
+        zombiespawn = transform.position;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(tiempodesalida >= tiempodeaparicion)
+        {
+            foreach (GameObject z in zombies)
+            {
+                tiempodesalida = 0;
+                if (z.activeInHierarchy == false)
+                {
+                    zombiespawn.x = Random.Range(ritghlimit.position.x, leftlimit.position.x);
+                    z.transform.position = zombiespawn;
+                    z.SetActive(true);
+                    break;
+                }
+            }
+        }
+        tiempodesalida += Time.deltaTime;
     }
 }
